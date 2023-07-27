1eab8ed8c2799d4f254bdcb8a9a436ca009d49aa
shrn-kira <shrnkira@gmail.com>
2023-07-27 00:49:53 +0000

    Adding the task 3 file

--- shell.h
+++ shell.h
@@ -13,5 +13,9 @@ void display_prompt(void);
 void read_command(char *command);
 void execute_command(const char *command);
 void execute_command(const char *command);
+char *find_command_path(const char *command);
+void execute_command_with_path(const char *full_path, const char *command);
+void execute_command(const char *command);
+void print_command_not_found(const char *command);
 
 #endif /* SHELL_H */
