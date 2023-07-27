97decf6ebe5f6c48a0a3745d1219219f05d0e07b
Alfred Njeru <njeruchomba2@gmail.com>
2023-07-26 19:20:11 -0700

    exit updates

---
+++ exit.c
@@ -0,0 +1,99 @@
+#include "shell.h"
+
+/**
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
+/**
+ * parse_exit_arguments - Parse arguments for the built-in "exit" command.
+ * @arg: The argument provided to the "exit" command.
+ * Return: The exit status as an integer.
+ * If the argument is invalid or not provided, return -1.
+ */
+int parse_exit_arguments(char *arg)
+{
+	int status = 0;
+	char *endptr;
+
+	if (!arg)
+		return (-1);
+
+	/* Convert the argument to an integer using strtol */
+	status = strtol(arg, &endptr, 10);
+
+	/* Check if the conversion was successful */
+	/* if the endptr points to the end of the string */
+	if (*endptr != '\0')
+		return (-1);
+
+	return (status);
+}
+
